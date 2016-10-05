c9f252a2396ce8aaeb3a45f6e94364198d8dd1ce
aisteron <timotheus@list.ru>
2016-10-05 12:34:20 +0300

    added swiper

---
+++ dev/js/custom.js
@@ -0,0 +1,12 @@
+/*swiper init*/
+$(document).ready(function(){
+var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        spaceBetween: 30
+    });
+	
+})
+    
